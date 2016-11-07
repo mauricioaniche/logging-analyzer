c5e939456474842ef49cb3a6fa52cf6176702a51
Maurício Aniche <mauricioaniche@gmail.com>
2016-11-07 12:21:48 +0100

    stats

--- stats/rq0.R
+++ stats/rq0.R
@@ -1,15 +1,12 @@
+# have you loaded 0-load?
+
+######## RQ0
+
 outersect = function(x, y) {
   sort(c(setdiff(x, y),
          setdiff(y, x)))
 }
 
-######### CONFIG
-
-logs <- read.csv("/Users/mauricioaniche/Desktop/log-study/apache/0all-logs.csv", header=T)
-projects <- read.csv("apache-projects.csv", header=T)
-
 nrow(projects) # total of projects in apacht
 length(unique(logs$project)) # total of analyzed projects
-
-######### 
 
