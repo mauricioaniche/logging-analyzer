38d7841795cda7fb989be8cee32cd56e58453ec1
Maurício Aniche <mauricioaniche@gmail.com>
2016-11-08 15:35:02 +0100

    removing r stats from here

--- stats/log-level-changes.R
+++
@@ -1,9 +0,0 @@
-######### CONFIG
-
-project <- "1all"
-directory <- "/Users/mauricioaniche/Desktop/log-study/apache"
-
-######### 
-
-changes <- read.csv(paste(directory, project, "-log-level-changes.csv", sep=""), header=T)
-table(changes$direction) # q7
